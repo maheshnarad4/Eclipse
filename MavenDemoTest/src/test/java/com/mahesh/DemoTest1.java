e4463e172020f542ed3e6a42591cad9d8f1f2566
Mahesh <Mahesh@192.168.0.5>
2021-06-09 02:08:45 +0530

    first commit

---
+++ MavenDemoTest/src/test/java/com/mahesh/DemoTest1.java
@@ -0,0 +1,24 @@
+package com.mahesh;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class DemoTest1 {
+
+	@Test
+	public void sum()
+	{
+		System.out.println("Sum Method");
+		int a=10,b=5;
+		Assert.assertEquals(15, a+b);
+	}
+	
+	@Test
+	public void sum1()
+	{
+		System.out.println("Sum1 Method");
+		int a=10,b=5;
+		Assert.assertEquals(5, a-b);
+	}
+}
